ebd7a4de0260b8f69be0095da9be612f4b09f3ec
sandra <sandrasaval11@gmail.com>
2020-12-27 02:59:16 -0800

    second commit

---
+++ src/app/crowd/crowd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crowd',
+  templateUrl: './crowd.component.html',
+  styleUrls: ['./crowd.component.css']
+})
+export class CrowdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
